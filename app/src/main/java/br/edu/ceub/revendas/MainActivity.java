5451fd996c6d6bd2c0b76adb9cd1a3768afa9e2e
william.teixeira <william.teixeira@sempreceub.com>
2019-11-07 21:59:20 -0300

    criar segunda activity

--- app/src/main/java/br/edu/ceub/revendas/MainActivity.java
+++ app/src/main/java/br/edu/ceub/revendas/MainActivity.java
@@ -2,13 +2,29 @@ package br.edu.ceub.revendas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btProdutos, btclientes, BtPagamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btProdutos = findViewById(R.id.btnProdutos);
+        btProdutos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuProdutos.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
